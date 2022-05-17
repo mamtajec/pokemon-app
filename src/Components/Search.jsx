bd341ee4666767d05eba8fdcc3a8a58eadb5bbbe
Mamta <mamtajec@gmail.com>
2022-05-17 12:13:56 +0530

    Keyboard accessible search

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,17 +1,26 @@
-import React, {useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function Search(props) {
 
-    const {handleSearch, resetFilters} = props
-    const [inputValue, setInputValue ] = useState("")
+  const { handleSearch, resetFilters } = props
+  const [inputValue, setInputValue] = useState("")
 
-    useEffect(() => {
-        !inputValue && resetFilters();
-      }, [inputValue]);
+  useEffect(() => {
+    !inputValue && resetFilters();
+
+    var input = document.getElementById("myInput");
+
+    input.addEventListener("keypress", function (event) {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        handleSearch(inputValue)
+      }
+    });
+  }, [inputValue]);
 
 
   return <div className='search-wrapper'>
-    <input type="search" value={inputValue} onChange={e=>setInputValue(e.target.value)} />
+    <input type="search" id="myInput" value={inputValue} onChange={e => setInputValue(e.target.value)} />
     <button onClick={() => handleSearch(inputValue)}>Search</button>
   </div>
 }
